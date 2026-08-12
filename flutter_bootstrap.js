ff52f9f3c551eae92f144f62c6d0601589cad932
SadidSD <122972806+SadidSD@users.noreply.github.com>
2026-08-12 07:20:32 +0000

    Deploying to gh-pages from @ SadidSD/Avan-App@c2c4fde96f9514067c37d66933811c6b647eb9b0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2646416959" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2561175309" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
