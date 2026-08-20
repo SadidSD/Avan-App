bd3db278872329479e8e7a9abe3ddb7cd047290e
SadidSD <sadidcoc123@gmail.com>
2026-08-20 12:48:01 +0600

    Fix root build.gradle.kts and upload verbose build logs in AAB workflow

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,14 +5,6 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
 subprojects {
     if (project.name != "app") {
         project.evaluationDependsOn(":app")
